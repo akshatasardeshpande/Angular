b90e447a699b63efad627eb9ddc6cdd300128356
Akshata <akshatasag@gmail.com>
2023-02-01 11:40:43 +0530

    added switch cases

--- Tic-Tac-Toee/src/app/components/icon/icon.component.ts
+++ Tic-Tac-Toee/src/app/components/icon/icon.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { faPen, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faCircle } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-icon',
   templateUrl: './icon.component.html',
-  styleUrls: ['./icon.component.css']
+  styleUrls: ['./icon.component.css'],
 })
-export class IconComponent {
+export class IconComponent implements OnInit {
+  @Input() iconName: string = '';
 
+  faPen = faPen;
+  faTimes = faTimes;
+  faCircle = faCircle;
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
