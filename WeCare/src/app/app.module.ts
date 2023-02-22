6dfbee054d6961176649e4469d750f7a7ccf4ffc
Akshata <akshatasag@gmail.com>
2023-02-22 12:40:46 +0530

    components creation

--- WeCare/src/app/app.module.ts
+++ WeCare/src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoachComponent } from './coach/coach.component';
+import { CoachhomeComponent } from './coach/coachhome/coachhome.component';
+import { CoachprofileComponent } from './coach/coachprofile/coachprofile.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { UserComponent } from './user/user.component';
+import { AppointmentsComponent } from './user/appointments/appointments.component';
+import { UserhomeComponent } from './user/userhome/userhome.component';
+import { UserprofileComponent } from './user/userprofile/userprofile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoachComponent,
+    CoachhomeComponent,
+    CoachprofileComponent,
+    HomeComponent,
+    LoginComponent,
+    SignupComponent,
+    UserComponent,
+    AppointmentsComponent,
+    UserhomeComponent,
+    UserprofileComponent
   ],
   imports: [
     BrowserModule,
