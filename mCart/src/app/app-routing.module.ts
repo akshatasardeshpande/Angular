7ac64b632cf663a29792f752e1dc48a764d54405
Akshata <akshatasag@gmail.com>
2023-02-27 19:06:24 +0530

    bug fixing

--- mCart/src/app/app-routing.module.ts
+++ mCart/src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './login/login.component';
+
+// Routing configuration
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'products', loadChildren: () => import('./products/products.module').then(m=>m.ProductsModule) },
+  { path: '', redirectTo: '/welcome', pathMatch: 'full' }
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
--- mCart/src/app/app.module.ts
+++ mCart/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ProductsComponent } from './products/products.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { CartComponent } from './products/cart/cart.component';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProductsComponent,
+    WelcomeComponent,
+    CartComponent,
+    // ProductDetailComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
