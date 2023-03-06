40d9f0b7d79648968ca2fb351896acb4a1f8ce35
Akshata <akshatasag@gmail.com>
2023-03-06 21:07:01 +0530

    generated bookridecomponent directive

---
+++ PoolCarz/src/app/book-ride-component.directive.spec.ts
@@ -0,0 +1,8 @@
+import { BookRideComponentDirective } from './book-ride-component.directive';
+
+describe('BookRideComponentDirective', () => {
+  it('should create an instance', () => {
+    const directive = new BookRideComponentDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ PoolCarz/src/app/book-ride-component.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appBookRideComponent]'
+})
+export class BookRideComponentDirective {
+
+  constructor() { }
+
+}
