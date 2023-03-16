00530bc06f05b40d7b9a0279a1b9d5ddafaaa014
Akshata <akshatasag@gmail.com>
2023-03-16 20:17:10 +0530

    fixed login component error

--- PoolCarz/src/app/app.module.ts
+++ PoolCarz/src/app/app.module.ts
@@ -1,22 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { BookRideComponentDirective } from './book-ride-component.directive';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    BookRideComponentDirective
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
-  bootstrap: [LoginComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
