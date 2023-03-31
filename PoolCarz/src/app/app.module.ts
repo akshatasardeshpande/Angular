d86bff4831fccd7fae50bbab0c653e3673b7a12a
Akshata <akshatasag@gmail.com>
2023-03-31 17:53:35 +0530

    added bookridecomponent

--- PoolCarz/src/app/app.module.ts
+++ PoolCarz/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { BookRideComponent } from './book-ride/book-ride.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    BookRideComponent
   ],
   imports: [
     BrowserModule,
--- PoolCarz/src/app/book-ride-component.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BookRideComponentDirective } from './book-ride-component.directive';
-
-describe('BookRideComponentDirective', () => {
-  it('should create an instance', () => {
-    const directive = new BookRideComponentDirective();
-    expect(directive).toBeTruthy();
-  });
-});
