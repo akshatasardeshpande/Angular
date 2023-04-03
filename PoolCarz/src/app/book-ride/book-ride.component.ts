9f25997d7d66c069bc73826897346800da1457e3
Akshata <akshatasag@gmail.com>
2023-04-03 17:15:04 +0530

    Stored the ride details in an array & rendered it in a table.

--- PoolCarz/src/app/book-ride/book-ride.component.ts
+++ PoolCarz/src/app/book-ride/book-ride.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-book-ride',
   templateUrl: './book-ride.component.html',
   styleUrls: ['./book-ride.component.css']
 })
-export class BookRideComponent {
+
+export class BookRideComponent implements OnInit {
+
+ShowAllRides : any = [];
+
+ngOnInit() {
+  
+  this.ShowAllRides = [
+    {
+    StartPoint: 'Vanrose Junction',
+    EndPoint: 'Office',
+    SeatsAvailable : 3,
+    },
+    {
+    StartPoint: 'PTP',
+    EndPoint: 'Office',
+    SeatsAvailable : 2,
+    },
+    {
+    StartPoint: 'Office',
+    EndPoint: 'East-Fort',
+    SeatsAvailable : 7,
+    },
+    {
+    StartPoint: 'Office',
+    EndPoint: 'Central Mall',
+    SeatsAvailable : 5,
+    },
+  ]
+}
 
 }
+
