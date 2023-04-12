4e1bd975bd582a73fc7db67149f288120b3627c9
Akshata <akshatasag@gmail.com>
2023-04-12 15:00:30 +0530

    added environment code

---
+++ github-finder-app/src/app/environment.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBViJz7KfKT74y8tlxV_xCLAgQY5R0iyno",
+  authDomain: "react30-c01d4.firebaseapp.com",
+  projectId: "react30-c01d4",
+  storageBucket: "react30-c01d4.appspot.com",
+  messagingSenderId: "962758137687",
+  appId: "1:962758137687:web:7a645e290647e306835012"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
