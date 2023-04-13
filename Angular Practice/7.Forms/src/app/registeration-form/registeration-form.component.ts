e7497c283c4cb4e946b400a3215688fb244ca502
Akshata <akshatasag@gmail.com>
2023-04-13 17:45:48 +0530

    initial commit

---
+++ Angular Practice/7.Forms/src/app/registeration-form/registeration-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registeration-form',
+  templateUrl: './registeration-form.component.html',
+  styleUrls: ['./registeration-form.component.css']
+})
+export class RegisterationFormComponent {
+
+}
