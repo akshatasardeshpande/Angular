c1f229cfdb2064bbf0c1c67396ebcb64702523c4
Akshata <akshatasag@gmail.com>
2023-04-13 19:07:25 +0530

    imported formbuilder & added few properties

--- Angular Practice/7b.CustomValidators/src/app/registration-form/registration-form.component.ts
+++ Angular Practice/7b.CustomValidators/src/app/registration-form/registration-form.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-registration-form',
   templateUrl: './registration-form.component.html',
   styleUrls: ['./registration-form.component.css']
 })
-export class RegistrationFormComponent {
+export class RegistrationFormComponent implements OnInit {
+
+  registerForm! : FormBuilder;
+  submitted! : boolean;
+
+  constructor(private formBuilder: FormBuilder){}
+
+ngOnInit(): void {
+  
+}
 
 }
