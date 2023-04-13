009aeadb7cbafb59b77d59a4f1b2537b04ea2645
Akshata <akshatasag@gmail.com>
2023-04-13 18:12:01 +0530

    added property in the ts file

--- Angular Practice/7.Forms/src/app/registeration-form/registeration-form.component.ts
+++ Angular Practice/7.Forms/src/app/registeration-form/registeration-form.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-registeration-form',
   templateUrl: './registeration-form.component.html',
   styleUrls: ['./registeration-form.component.css']
 })
-export class RegisterationFormComponent {
+export class RegisterationFormComponent implements OnInit {
+
+registerForm! : FormGroup;
+submitted!: Boolean;
+
+constructor(private formBuilder: FormBuilder){}
+
+  ngOnInit(): void {
+   this.registerForm = this.formBuilder.group({
+    firstName : ['', Validators.required],
+    lastName : ['', Validators.required],
+    address : this.formBuilder.group({
+      street: [],
+      zip : [],
+      city : []
+    })
+   }) ;
+  }
+
 
 }
