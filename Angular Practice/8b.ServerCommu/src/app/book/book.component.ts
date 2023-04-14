3b647979db60a52f3a231f39a462e8ac0d44449b
Akshata <akshatasag@gmail.com>
2023-04-14 12:18:25 +0530

    added properties

--- Angular Practice/8b.ServerCommu/src/app/book/book.component.ts
+++ Angular Practice/8b.ServerCommu/src/app/book/book.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {book} from './book';
+import { BookService } from './book.service';
 
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.css']
 })
-export class BookComponent {
+export class BookComponent implements OnInit {
+books!: book[];
+
+constructor(private bookService: BookService){}
+
+ngOnInit(): void {
+  this.books = this.bookService.getBook();
+}
 
 }
