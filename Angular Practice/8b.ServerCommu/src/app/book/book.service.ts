2333a14e766536e04a9bdfe26643715613522cb7
Akshata <akshatasag@gmail.com>
2023-04-14 12:12:44 +0530

    generated book service

---
+++ Angular Practice/8b.ServerCommu/src/app/book/book.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { BookComponent } from './book.component';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+
+  constructor() { }
+}
