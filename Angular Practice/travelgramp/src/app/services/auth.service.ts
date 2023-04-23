7594139c6c5d5e1d29b2597c4874bb3b1eaea9b7
Akshata <akshatasag@gmail.com>
2023-04-23 16:17:41 +0530

    adding auth service code

--- Angular Practice/travelgramp/src/app/services/auth.service.ts
+++ Angular Practice/travelgramp/src/app/services/auth.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {AngularFireAuth} from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private auth: AngularFireAuth) { }
+
+  signUp(email: string, password: string) {
+    return this.auth.createUserWithEmailAndPassword(email, password);
+  }
+
+  
+
 }
