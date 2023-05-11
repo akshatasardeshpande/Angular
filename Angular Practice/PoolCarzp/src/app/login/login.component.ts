4e9a0733bfca6b5b1afbc4b05c7d7392aa944b83
Akshata <akshatasag@gmail.com>
2023-05-11 20:17:50 +0530

    login component data binding

--- Angular Practice/PoolCarzp/src/app/login/login.component.ts
+++ Angular Practice/PoolCarzp/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { login } from './login';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-login',
@@ -14,21 +15,34 @@ export class LoginComponent implements OnInit  {
   loginForm!: FormGroup;
   valid = true;
   users : login[] = [];
-  
-  constructor(private formBuilder: FormBuilder)
-  {}
+  @ViewChild('uname') usernameElement!: ElementRef;
+
+  constructor(private formBuilder: FormBuilder,
+    private loginService: LoginService, private renderer: Renderer2 )
+  {
+
+  }
 
   ngOnInit() {
-  this.loginForm = this.formBuilder.group({
+    // Makes a service call to fetch users data from the backend
+    this.loginService.getUsers().subscribe({next:users => this.users = users});
+    this.loginForm = this.formBuilder.group({
 userName: [this.Login.uName, Validators.required],
 password: [this.Login.pwd, Validators.required]
   })
 }
 
+// Invoked when user clicks submit in login form
+    // Validates the credentials with the data fetched from the backend
 onSubmit(){
   //fetches the form object containing the values of all the form controls
   this.Login = this.loginForm.getRawValue();
-
+  const user = this.users.filter(currUser => currUser.uName === this.Login.uName && currUser.pwd === this.Login.pwd)[0];
+  if (user) {
+       this.loginService.username = this.Login.uName;      
+  } else {
+      this.valid = false;
+  }
 }
 
 
