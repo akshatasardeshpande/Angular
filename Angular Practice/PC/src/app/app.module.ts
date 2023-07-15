c68ff267348791085f9b77141306a1f1e7ecb49b
Akshata <akshatasag@gmail.com>
2023-07-15 19:57:37 +0530

    generated component ride-details

--- Angular Practice/PC/src/app/app.module.ts
+++ Angular Practice/PC/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { LoginComponent } from './login/login.component';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { loginService } from './services/login-service.service';
+import { RideDetailsComponent } from './ride-details/ride-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RideDetailsComponent
   ],
   imports: [
     BrowserModule,
