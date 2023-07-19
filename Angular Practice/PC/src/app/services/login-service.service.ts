8b9096f256bababec5c9810c0811d5fad11ebc40
Akshata <akshatasag@gmail.com>
2023-07-19 18:12:30 +0530

    deleted the comment

--- Angular Practice/PC/src/app/services/login-service.service.ts
+++ Angular Practice/PC/src/app/services/login-service.service.ts
@@ -8,6 +8,7 @@ import { LoginComponent } from '../login/login.component';
 @Injectable({
   providedIn: 'root'
 })
+
 export class loginService {
   Susername = '';
   loginElement!: ElementRef;
@@ -16,16 +17,17 @@ export class loginService {
   
   // Makes a get request to the backend to fetch users data
   getUsers(): Observable<loginModel[]>{
+    "Inside getUsers()"
     return this.http.get<loginModel[]>('./assets/users/users.json').pipe(
       catchError(this.handleError)
     );
   }
-  
-  // src\assets\users\users.json
+
    // Invoked if an error is thrown in the get request
    private handleError(err: HttpErrorResponse) {
     console.error(err);
     return throwError(()=>err.error() || 'Server error');
-}
+   }
 
+   
 }
