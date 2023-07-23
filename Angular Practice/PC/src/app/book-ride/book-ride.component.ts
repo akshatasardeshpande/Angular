ce579366ac5e958db654874b9ce4b01d664222c2
Akshata <akshatasag@gmail.com>
2023-07-23 20:33:55 +0530

    Updated book-ride component.ts file

--- Angular Practice/PC/src/app/book-ride/book-ride.component.ts
+++ Angular Practice/PC/src/app/book-ride/book-ride.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-book-ride',
   templateUrl: './book-ride.component.html',
   styleUrls: ['./book-ride.component.css']
 })
-export class BookRideComponent {
+
+export class BookRideComponent implements OnInit {
+
+ShowAllRides : any = [];
+showTable : boolean = false;
+
+ngOnInit() {
+  
+  this.ShowAllRides = [
+    {
+    StartPoint: 'Vanrose Junction',
+    EndPoint: 'Office',
+    SeatsAvailable : 3,
+    },
+    {
+    StartPoint: 'PTP',
+    EndPoint: 'Office',
+    SeatsAvailable : 2,
+    },
+    {
+    StartPoint: 'Office',
+    EndPoint: 'East-Fort',
+    SeatsAvailable : 7,
+    },
+    {
+    StartPoint: 'Office',
+    EndPoint: 'Central Mall',
+    SeatsAvailable : 5,
+    },
+  ]
+}
+// ShowAll($event: any){
+// if($event.target.innerHTMl == "")
+// {
+  
+// }
+// }
+
+clickEvent(){
+  if(this.showTable == false){
+    this.showTable= true;
+  }
+  else{
+    this.showTable= false;
+  }
 
 }
+
+}
+
